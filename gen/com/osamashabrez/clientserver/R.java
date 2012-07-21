4780afb1c5a7b9f7af5df12379847e9bf0b85d70
osamashabrez <osamashabrez@live.com>
2012-07-21 19:51:57 +0500

    Configured menu exit

--- gen/com/osamashabrez/clientserver/R.java
+++ gen/com/osamashabrez/clientserver/R.java
@@ -16,7 +16,7 @@ public final class R {
     }
     public static final class id {
         public static final int listView=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_exit=0x7f070001;
     }
     public static final class layout {
         public static final int main_activity=0x7f030000;
@@ -26,9 +26,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int menu_exit=0x7f040001;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- src/com/osamashabrez/clientserver/Main.java
+++ src/com/osamashabrez/clientserver/Main.java
@@ -2,7 +2,9 @@ package com.osamashabrez.clientserver;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Main extends Activity {
 
@@ -16,5 +18,22 @@ public class Main extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	try {
+    		switch (item.getItemId()) {
+    		case R.id.menu_exit:
+        		Log.d("Application exit", "exit selected from menu");
+        		System.exit(0);
+    			break;
+    		default:
+        		return super.onOptionsItemSelected(item);
+    		}
+    	} catch (Exception ex) {
+    		Log.d("Menu item error", ex.getMessage());
+    	}
+		return true;
     }
 }
