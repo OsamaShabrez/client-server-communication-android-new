2df4f64a1c1982171b8e44386e6e049386ad30ae
osamashabrez <osamashabrez@live.com>
2012-07-21 22:48:23 +0500

    Added about dialog box in menu

--- gen/com/osamashabrez/clientserver/R.java
+++ gen/com/osamashabrez/clientserver/R.java
@@ -16,7 +16,8 @@ public final class R {
     }
     public static final class id {
         public static final int listView=0x7f070000;
-        public static final int menu_exit=0x7f070001;
+        public static final int menu_about=0x7f070001;
+        public static final int menu_exit=0x7f070002;
     }
     public static final class layout {
         public static final int main_activity=0x7f030000;
@@ -26,8 +27,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int dialog_action_dismiss=0x7f040004;
+        public static final int dialog_action_title=0x7f040005;
+        public static final int menu_about=0x7f040002;
         public static final int menu_exit=0x7f040001;
-        public static final int title_activity_main=0x7f040002;
+        public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- src/com/osamashabrez/clientserver/Main.java
+++ src/com/osamashabrez/clientserver/Main.java
@@ -2,6 +2,9 @@ package com.osamashabrez.clientserver;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +27,18 @@ public class Main extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
     	try {
     		switch (item.getItemId()) {
+    		case R.id.menu_about:
+        		Log.d("Application about", "view dialouge about me");
+                Dialog dialog;
+            	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.dialog_action_title)
+                	.setMessage(Html.fromHtml("This tutorial on Android Client Server was written for <a href=\"http://osamashabrez.com\">OsamaShabrez.com</a>"))
+                	.setCancelable(false)
+                	.setIcon(android.R.drawable.ic_dialog_info)
+                	.setPositiveButton(R.string.dialog_action_dismiss, null);
+                dialog = builder.create();
+                dialog.show();
+    			break;
     		case R.id.menu_exit:
         		Log.d("Application exit", "exit selected from menu");
         		System.exit(0);
